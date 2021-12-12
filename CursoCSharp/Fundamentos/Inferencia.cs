7e71b07a9660f85f7a47482419585a461306e322
Danilo N. B <danilobenatti@gmail.com>
2021-12-12 20:32:21 -0300

    Inferencia de variávieis

---
+++ CursoCSharp/Fundamentos/Inferencia.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    class Inferencia {
+
+        public static void Executar() {
+            var nome = "Danilo";
+            //nome = 123;
+            Console.WriteLine(nome);
+
+            //int idade;
+            var idade = 39;
+            Console.WriteLine(idade);
+
+            int a = 3, b = 2;
+            Console.WriteLine(a + b);
+        }
+    }
+}
