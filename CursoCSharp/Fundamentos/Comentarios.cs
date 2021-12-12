7952531a561c17dcc3ce362da266296a61a45b8d
Danilo N. B <danilobenatti@gmail.com>
2021-12-12 18:50:30 -0300

    Add project files.

---
+++ CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.Fundamentos {
+    /// <summary>
+    /// 
+    /// </summary>
+    class Comentarios {
+        public static void Executar() {
+            // cuidado com os comentários desnecessários
+            Console.WriteLine("Código claro é sempre melhor!");
+
+            /*
+             *  Esse é um comentário
+             *  de múltiplas linhas
+             */
+            Console.WriteLine("O C# tem o XML Domments");
+        }
+
+    }
+}
