96267e8d26ef814784a1aae1f2a8aa89e6d6f41b
Danilo N. B <danilobenatti@gmail.com>
2021-12-12 23:04:33 -0300

    Notação Ponto

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -11,7 +11,8 @@ namespace CursoCSharp {
                 {"Comentários - Fundamentos", Comentarios.Executar},
                 {"Variáveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
                 {"Inferência - Fundamentos", Inferencia.Executar},
-                {"Interpolação - Fundamentos", Interpolacao.Executar}
+                {"Interpolação - Fundamentos", Interpolacao.Executar},
+                {"Notação Ponto - Fundamentos", NotacaoPonto.Executar}
             });
 
             central.SelecionarEExecutar();
