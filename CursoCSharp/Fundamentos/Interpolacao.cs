6338fd4dcc4a214a6b105614de8d6b38cb065a35
Danilo N. B <danilobenatti@gmail.com>
2021-12-12 22:40:31 -0300

    Interpolação

---
+++ CursoCSharp/Fundamentos/Interpolacao.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+
+namespace CursoCSharp.Fundamentos {
+    class Interpolacao {
+        public static void Executar() {
+            string nome = "Notebook Gamer";
+            string marca = "Dell";
+            double preco = 5800;
+
+            Console.WriteLine("O " + nome + " da marca " + marca + " custa " + preco);
+            Console.WriteLine("O {0} da marca {1} custa {2}.", nome, marca, preco.ToString("C2", CultureInfo.CreateSpecificCulture("pt-BR")));
+            Console.WriteLine($"A marca {marca} é muito boa!");
+            Console.WriteLine($"1 + 1 = {1 + 1}!");
+        }
+    }
+}
