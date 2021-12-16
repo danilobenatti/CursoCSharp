cbdc9a13f48e22b60c8c64f88c65dfa62cc28647
Danilo N. B <danilobenatti@gmail.com>
2021-12-16 11:23:35 -0300

    Operadores Unários

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -19,7 +19,8 @@ namespace CursoCSharp {
                 {"Operadores Aritméticos - Fundamentos", OperadoresAritmeticos.Executar},
                 {"Operadores Relacionais - Fundamentos", OperadoresRelacionais.Executar},
                 {"Operadores Lógicos - Fundamentos", OperadoresLogicos.Executar},
-                {"Operadores de Atribuição - Fundamentos", OperadoresAtribuicao.Executar}
+                {"Operadores de Atribuição - Fundamentos", OperadoresAtribuicao.Executar},
+                {"Operadores Unários - Fundamentos", OperadoresUnarios.Executar}
             });
 
             central.SelecionarEExecutar();
