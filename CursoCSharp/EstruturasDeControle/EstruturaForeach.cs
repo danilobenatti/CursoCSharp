dd20b53cf13d529a40f4190a5f32656bbbebb0d2
Danilo N. B <danilobenatti@gmail.com>
2021-12-17 09:04:09 -0300

    Estrutura Foreach

---
+++ CursoCSharp/EstruturasDeControle/EstruturaForeach.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CursoCSharp.EstruturasDeControle {
+    class EstruturaForEach {
+        public static void Executar() {
+            var palavra = "Opa!";
+
+            foreach (var letra in palavra) {
+                Console.WriteLine(letra);
+            }
+
+            var alunos = new string[] { "Ana", "Maria", "Carlos" };
+            foreach (var nome in alunos) {
+                Console.WriteLine(nome);
+            }
+
+        }
+    }
+}
