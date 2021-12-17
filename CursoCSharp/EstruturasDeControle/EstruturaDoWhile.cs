536eb4e780400308f6a69217469538aa343b41f9
Danilo N. B <danilobenatti@gmail.com>
2021-12-16 22:57:50 -0300

    Estrutura Do While

---
+++ CursoCSharp/EstruturasDeControle/EstruturaDoWhile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace CursoCSharp.EstruturasDeControle {
+    class EstruturaDoWhile {
+        public static void Executar() {
+            string entrada;
+
+            do {
+                Console.WriteLine("Qual o seu nome: ");
+                entrada = Console.ReadLine();
+
+                Console.WriteLine($"Seja bem vindo(a)! {entrada}.");
+                Console.Write("Deseja continuar?(S/N) ");
+                entrada = Console.ReadLine();
+
+            } while (entrada.ToLower() == "s");
+        }
+    }
+}
