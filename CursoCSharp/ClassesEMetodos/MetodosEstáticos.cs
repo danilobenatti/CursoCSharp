3685de5dc80defa72b9ba66b1cf260979d49e8f7
Danilo N. B <danilobenatti@gmail.com>
2021-12-18 15:48:36 -0300

    Métodos Estáticos

---
+++ CursoCSharp/ClassesEMetodos/MetodosEstáticos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CursoCSharp.ClassesEMetodos {
+    public class CalculadoraEstatica {
+
+        //Método de Classe ou Método Estático.
+        public static int Multiplicar(int a, int b) => a * b;
+        
+        //Método de instância.
+        public int Somar(int a, int b) => a + b;
+    }
+    class MetodosEstáticos {
+        public static void Executar() {
+            var resultado = CalculadoraEstatica.Multiplicar(2, 3);
+            Console.WriteLine($"Resultato da multiplicação: {resultado}");
+
+            var calculadoraEstatica = new CalculadoraEstatica();
+            Console.WriteLine($"Resultado da soma: {calculadoraEstatica.Somar(2, 3)}");
+        }
+    }
+}
