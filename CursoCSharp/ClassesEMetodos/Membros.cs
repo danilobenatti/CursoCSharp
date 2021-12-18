6ef65f76d660d72a3d0fc823abc2892901919c49
Danilo N. B <danilobenatti@gmail.com>
2021-12-17 21:01:22 -0300

    Membros - Atributos e Métodos

---
+++ CursoCSharp/ClassesEMetodos/Membros.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CursoCSharp.ClassesEMetodos {
+    class Membros {
+        public static void Executar() {
+            Pessoa pessoa1 = new Pessoa();
+            pessoa1.Nome = "João";
+            pessoa1.Idade = 38;
+            pessoa1.Sexo = 'M';
+            pessoa1.Ativo = true;
+
+            var pessoa2 = new Pessoa() { Nome = "Maria", Idade = 35, Sexo = 'F', Ativo = false };
+
+            Console.WriteLine($"Apresentação de { pessoa1.Nome}");
+            pessoa1.ApresentarNoConsole();
+            Console.WriteLine($"Apresentação de { pessoa2.Nome}");
+            pessoa2.ApresentarNoConsole();
+        }
+    }
+}
---
+++ CursoCSharp/ClassesEMetodos/Pessoa.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Timers;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.ClassesEMetodos {
+    class Pessoa {
+        public string Nome;
+        public int Idade;
+        public char Sexo;
+        public bool Ativo;
+
+        public string Apresentar() {
+            return string.Format("Olá me chamo " + Nome + " e tenho " + Idade + " anos.");
+        }
+
+        public void ApresentarNoConsole() {
+            Console.WriteLine(Apresentar());
+        }
+    }
+}
